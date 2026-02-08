059920a9692e21178323da8f806108b1f428e6c1
bsnbanjau-design <bsnbanjau@gmail.com>
2026-02-08 00:06:57 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.sessionid = process.env.SESSION_ID || '';
+global.sessionid = process.env.SESSION_ID || '{"noiseKey":{"private":{"type":"Buffer","data":"qJTCfBqNd/eyKVeNnTRG2AQdy2RLEZ3DnRE5eTAE6n0="},"public":{"type":"Buffer","data":"1Ck0WW4L6vI8rs01l/5GyrVuLud7DnZjtk/asFKmxUY="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"wFKrZFUQMFstXamvOVU7l7egq2xhgEJVPq6Cts6bEUg="},"public":{"type":"Buffer","data":"wpfw3BwNpDutwBGslZWb0QP7O6Y0s8A+90tnRT2tsBA="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"YDAYuSxoT4lvyh4Us5jmLqJ4Y4C0z14hZzWjNpct1ko="},"public":{"type":"Buffer","data":"yoY/o85Ffb09jXzO56GkDdzvp9/+iYbTrR0+buukfAs="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"4HaoI8BS0v3UOjHQe64uH7K81XlUaiO5w41RkpuY0Vs="},"public":{"type":"Buffer","data":"B8AVpJtb4Uw7PpbvSYCilRAf0KlK1MUTyfMAjDvlcgs="}},"signature":{"type":"Buffer","data":"SgLawSA2W0/J2qePm51eFbqv1WQhBPZ5PCd8BwXe5zismY+PV+JdKMGvLnSf1D2BlP2phpD3CRtULBr6oYeZCg=="},"keyId":1},"registrationId":226,"advSecretKey":"Tc8J5YRyVFreXo3O2YQRBZ21YznDPGSB9sIqL/ey+So=","processedHistoryMessages":[],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"registered":false,"account":{"details":"CNyowLcFEPaloMwGGAMgACgA","accountSignatureKey":"pijYZOWYHSv0Uo1uS7eWOPM9MAifKrYyrb6oKtPmUDk=","accountSignature":"ykjTXJv37UA3nT2h2YCNbUkTiVF+2tb2oStqOaQKFx+VshVEns1fGwxjVbD1cT+Pn0F9T7of61oX5eDhPZMEDg==","deviceSignature":"nJKzjxP7d0uEmxW2Gis4dp/sKSfQO6vdoViXheB86r0dBS2nk1NIC0cy7tOqlenCf/PoTaxnPAb+/oh4I765Bw=="},"me":{"id":"447463562170:19@s.whatsapp.net","lid":"219537974800514:19@lid"},"signalIdentities":[{"identifier":{"name":"447463562170:19@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BaYo2GTlmB0r9FKNbku3ljjzPTAInyq2Mq2+qCrT5lA5"}}],"platform":"android","routingInfo":{"type":"Buffer","data":"CAgIDQgF"},"lastAccountSyncTimestamp":1770525443,"lastPropHash":"3R9Z39","myAppStateKeyId":"AAAAAC7t"}';
 global.ytname = process.env.YT_NAME || "YT: @EliteProTechs";
 global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
 global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '2347047504860';
-global.ownername = process.env.OWNER_NAME || 'ElitePro';
-global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
+global.ownernumber = process.env.OWNER_NUMBER || '447463562170';
+global.ownername = process.env.OWNER_NAME || 'NABEEL';
+global.botname = process.env.BOT_NAME || 'COPRONYX-BOT';
 
 // Default settings 
 global.prefix = process.env.PREFIX || '.';
@@ -28,7 +28,7 @@ global.autoreact = process.env.AUTO_REACT === 'true';
 global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'true';
 
 // Thumbnail profile picture
-global.elitepropp = process.env.ELITE_PRO_PP || 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg';
+global.elitepropp = process.env.ELITE_PRO_PP || 'https://i.postimg.cc/PJqh4Xz3/COPRONYX-COP-ESPORTS-20260207-192245.jpg';
 // Default emoji
 global.themeemoji = process.env.THEME_EMOJI || '👨‍💻';
 
